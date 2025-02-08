d4e7a5018f10915f6507524b75a71380d6f95f32
asmaaHamdym <asmahamdym@gmail.com>
2025-02-09 00:33:43 +0200

    added book model to db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json());
+const port = 3000 || process.env.PORT;
 dotenv.config();
 
 app.get("/", (req, res) => {
-  console.log(req);
-
   return res.status(234).send("Hello from my server");
 });
-const port = 3000 || process.env.PORT;
+
+// post request to add a book
+app.post("/books", async (req, res) => {
+  console.log(req.body);
+
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send("Please fill all required fields: title, author, publishYear");
+    }
+    const newBook = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    });
+    const newBookCreated = await Book.create(newBook);
+    return res.status(201).send(newBookCreated);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Internal server error");
+  }
+});
 
 mongoose
   .connect(process.env.MONGO_CONNECT_STRING)
