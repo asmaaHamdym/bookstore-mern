45b8b21ba8880dafef90447b469d57ce52b39064
asmaaHamdym <asmahamdym@gmail.com>
2025-02-10 23:59:51 +0200

    Added basic react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import EditBook from "./pages/EditBook";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import ShowBook from "./pages/ShowBook";
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/edit" element={<EditBook />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/delete" element={<DeleteBook />} />
+      <Route path="/books/show" element={<ShowBook />} />
+    </Routes>
+  );
 };
