d5c67a812ec840837a99ccc1bf503188d6409b49
asmaaHamdym <asmahamdym@gmail.com>
2025-03-01 20:04:01 +0200

    added back button and show book components

---
+++ frontend/src/components/Backbutton.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
+
+export const Backbutton = ({ destination = "/" }) => {
+  return (
+    <div className="flex ">
+      <Link
+        to={destination}
+        className="bg-sky-800 text-white  py-1 px-4 rounded-lg w-fit"
+      >
+        <MdArrowBack className="text-2xl" />
+      </Link>
+    </div>
+  );
+};
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,4 +1,63 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { Backbutton } from "../components/Backbutton";
+
 const ShowBook = () => {
-  return <div>ShowBook</div>;
+  const { id } = useParams();
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/books/${id}`)
+      .then((res) => {
+        console.log(res);
+        setBook(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div className="p-4">
+      <Backbutton />
+      <h1 className="text-3xl my-8">Book Details</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fir p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Id</span>
+            <span>{book._id}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Creation Time</span>
+            <span>{book.createdAt}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray=500">Last Update Time</span>
+            <span>{book.updatedAt}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 export default ShowBook;
