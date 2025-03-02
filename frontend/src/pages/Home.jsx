08dbed17d18e39f31885343334ec2a792f4b8893
asmaaHamdym <asmahamdym@gmail.com>
2025-03-03 00:11:33 +0200

    added view options to homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Spinner from "../components/Spinner";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 import { BooksTable } from "../components/home/BooksTable";
 import { BooksCard } from "../components/home/BooksCard";
@@ -11,6 +9,7 @@ import { BooksCard } from "../components/home/BooksCard";
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [view, setView] = useState("table");
   useEffect(() => {
     setLoading(true);
     axios
@@ -29,6 +28,21 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setView("table")}
+        >
+          Table
+        </button>
+
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+          onClick={() => setView("card")}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link
@@ -40,7 +54,7 @@ const Home = () => {
         </Link>
       </div>
       {loading && <Spinner />}
-      <BooksTable books={books} />
+      {view == "table" ? <BooksTable books={books} /> : <BooksCard />}
     </div>
   );
 };
